28daf6e7d4497d38d3c0c466de9759aedc2baddb
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2020-04-14 00:45:59 +0200

    Add getParameters() support (sender&receiver)

--- WebRTCPlugin/RTPReceiver.cpp
+++ WebRTCPlugin/RTPReceiver.cpp
@@ -63,7 +63,138 @@ STDMETHODIMP RTPReceiver::getParameters(VARIANT* params)
 {
   if (!receiver)
     return E_UNEXPECTED;
-  //TODO
+
+  CComSafeArray<VARIANT> args;
+
+  //get parameters
+  auto recvparams = receiver->GetParameters();
+
+  /*
+  required sequence<RTCRtpHeaderExtensionParameters> headerExtensions;
+  required RTCRtcpParameters rtcp;
+  required sequence<RTCRtpCodecParameters> codecs;
+  */
+
+  //Add header extensions
+  {
+    CComSafeArray<VARIANT> extensions;
+    //Create empty array
+    extensions.Create();
+    //For each one
+    for (auto& extension : recvparams.header_extensions)
+    {
+      /*
+        required DOMString uri;
+        required unsigned short id;
+        boolean encrypted = false;
+      */
+      CComSafeArray<VARIANT> arr;
+      arr.Add(variant_t(extension.uri.c_str()));
+      arr.Add(variant_t(extension.id));
+      arr.Add(variant_t(extension.encrypt));
+
+      // Initialize the variant
+      VARIANT var;
+      VariantInit(&var);
+      var.vt = VT_ARRAY | VT_VARIANT;
+      var.parray = arr.Detach();
+      //add to encodinsg
+      extensions.Add(var);
+    }
+    // Initialize the variant
+    VARIANT var;
+    VariantInit(&var);
+    var.vt = VT_ARRAY | VT_VARIANT;
+    var.parray = extensions.Detach();
+
+    //Add encodings
+    args.Add(var);
+  }
+
+
+  //Add rtcp stuff
+  {
+    /*
+          DOMString cname;
+          boolean reducedSize;
+    */
+    CComSafeArray<VARIANT> arr;
+    arr.Add(variant_t(recvparams.rtcp.cname.c_str()));
+    arr.Add(variant_t(recvparams.rtcp.reduced_size));
+
+    // Initialize the variant
+    VARIANT var;
+    VariantInit(&var);
+    var.vt = VT_ARRAY | VT_VARIANT;
+    var.parray = arr.Detach();
+    //add to encodinsg
+    args.Add(var);
+  }
+
+  //Add codecs
+  {
+    CComSafeArray<VARIANT> codecs;
+    //Create empty array
+    codecs.Create();
+    //For each one
+    for (auto& codec : recvparams.codecs)
+    {
+      /**
+        required octet payloadType;
+        required DOMString mimeType;
+        required unsigned long clockRate;
+        unsigned short channels;
+        DOMString sdpFmtpLine;
+      */
+      CComSafeArray<VARIANT> arr;
+      arr.Add(variant_t(codec.payload_type));
+      arr.Add(variant_t(codec.mime_type().c_str()));
+      if (codec.clock_rate.has_value())
+        arr.Add(variant_t(codec.clock_rate.value()));
+      else
+        arr.Add(variant_t());
+      if (codec.num_channels.has_value())
+        arr.Add(variant_t(codec.num_channels.value()));
+      else
+        arr.Add(variant_t());
+      if (!codec.parameters.empty())
+      {
+        std::string sdpFmtpLine;
+        for (auto& pair : codec.parameters)
+        {
+          if (!sdpFmtpLine.empty())
+            sdpFmtpLine += "; ";
+          sdpFmtpLine += pair.first + "=" + pair.second;
+        }
+        arr.Add(variant_t(sdpFmtpLine.c_str()));
+      } else {
+        arr.Add(variant_t());
+      }
+
+      // Initialize the variant
+      VARIANT var;
+      VariantInit(&var);
+      var.vt = VT_ARRAY | VT_VARIANT;
+      var.parray = arr.Detach();
+      //add to encodinsg
+      codecs.Add(var);
+    }
+    // Initialize the variant
+    VARIANT var;
+    VariantInit(&var);
+    var.vt = VT_ARRAY | VT_VARIANT;
+    var.parray = codecs.Detach();
+
+    //Add codecs to params
+    args.Add(var);
+  }
+
+  // Initialize the variant
+  VariantInit(params);
+  params->vt = VT_ARRAY | VT_VARIANT;
+  params->parray = args.Detach();
+
+  //Done
   return S_OK;
 };
 
--- WebRTCPlugin/RTPSender.cpp
+++ WebRTCPlugin/RTPSender.cpp
@@ -1,6 +1,9 @@
 // RTPSender.cpp : Implementation of RTPSender
 #include "stdafx.h"
+#include <atlsafe.h>
 
+// this project
+#include "JSObject.h"
 #include "RTPSender.h"
 #include "MediaStreamTrack.h"
 
@@ -54,6 +57,7 @@ STDMETHODIMP RTPSender::setParameters(VARIANT params)
 {
   if (!sender)
     return E_UNEXPECTED;
+
   //TODO
   return S_OK;
 };
@@ -62,7 +66,186 @@ STDMETHODIMP RTPSender::getParameters(VARIANT* params)
 {
   if (!sender)
     return E_UNEXPECTED;
-  //TODO
+
+  CComSafeArray<VARIANT> args;
+
+  //get parameters
+  auto sendparams = sender->GetParameters();
+
+  /*
+  required DOMString transactionId;
+  required sequence<RTCRtpEncodingParameters> encodings;
+  required sequence<RTCRtpHeaderExtensionParameters> headerExtensions;
+  required RTCRtcpParameters rtcp;
+  required sequence<RTCRtpCodecParameters> codecs;
+  */
+  //Add params
+  args.Add(variant_t(sendparams.transaction_id.c_str()));
+  
+  //Add encodings
+  {
+    CComSafeArray<VARIANT> encodings;
+    //Create empty array
+    encodings.Create();
+    //For each one
+    for (auto& encoding : sendparams.encodings)
+    {
+      /*
+        DOMString rid;
+        boolean active = true;
+        unsigned long maxBitrate;
+        double scaleResolutionDownBy;
+      */
+      CComSafeArray<VARIANT> arr;
+      arr.Add(variant_t(encoding.rid.c_str()));
+      arr.Add(variant_t(encoding.active));
+      if (encoding.max_bitrate_bps.has_value())
+        arr.Add(variant_t(encoding.max_bitrate_bps.value()));
+      else
+        arr.Add(variant_t());
+      if (encoding.scale_framerate_down_by.has_value())
+        arr.Add(variant_t(encoding.scale_framerate_down_by.value()));
+      else
+        arr.Add(variant_t());
+      
+      // Initialize the variant
+      VARIANT var;
+      VariantInit(&var);
+      var.vt = VT_ARRAY | VT_VARIANT;
+      var.parray = arr.Detach();
+      //add to encodinsg
+      encodings.Add(var);
+    }
+    // Initialize the variant
+    VARIANT var;
+    VariantInit(&var);
+    var.vt = VT_ARRAY | VT_VARIANT;
+    var.parray = encodings.Detach();
+
+    //Add encodings
+    args.Add(var);
+   }
+
+  //Add header extensions
+  {
+    CComSafeArray<VARIANT> extensions;
+    //Create empty array
+    extensions.Create();
+    //For each one
+    for (auto& extension : sendparams.header_extensions)
+    {
+      /*
+        required DOMString uri;
+        required unsigned short id;
+        boolean encrypted = false;
+      */
+      CComSafeArray<VARIANT> arr;
+      arr.Add(variant_t(extension.uri.c_str()));
+      arr.Add(variant_t(extension.id));
+      arr.Add(variant_t(extension.encrypt));
+
+      // Initialize the variant
+      VARIANT var;
+      VariantInit(&var);
+      var.vt = VT_ARRAY | VT_VARIANT;
+      var.parray = arr.Detach();
+      //add to encodinsg
+      extensions.Add(var);
+    }
+    // Initialize the variant
+    VARIANT var;
+    VariantInit(&var);
+    var.vt = VT_ARRAY | VT_VARIANT;
+    var.parray = extensions.Detach();
+
+    //Add encodings
+    args.Add(var);
+  }
+  
+
+  //Add rtcp stuff
+  {
+    /*
+          DOMString cname;
+          boolean reducedSize;
+    */
+    CComSafeArray<VARIANT> arr;
+    arr.Add(variant_t(sendparams.rtcp.cname.c_str()));
+    arr.Add(variant_t(sendparams.rtcp.reduced_size));
+
+    // Initialize the variant
+    VARIANT var;
+    VariantInit(&var);
+    var.vt = VT_ARRAY | VT_VARIANT;
+    var.parray = arr.Detach();
+    //add to encodinsg
+    args.Add(var);
+  }
+
+  //Add codecs
+  {
+    CComSafeArray<VARIANT> codecs;
+    //Create empty array
+    codecs.Create();
+    //For each one
+    for (auto& codec : sendparams.codecs)
+    {
+      /**
+        required octet payloadType;
+        required DOMString mimeType;
+        required unsigned long clockRate;
+        unsigned short channels;
+        DOMString sdpFmtpLine;
+      */
+      CComSafeArray<VARIANT> arr;
+      arr.Add(variant_t(codec.payload_type));
+      arr.Add(variant_t(codec.mime_type().c_str()));
+      if (codec.clock_rate.has_value())
+        arr.Add(variant_t(codec.clock_rate.value()));
+      else
+        arr.Add(variant_t());
+      if (codec.num_channels.has_value())
+        arr.Add(variant_t(codec.num_channels.value()));
+      else
+        arr.Add(variant_t());
+      if (!codec.parameters.empty())
+      {
+        std::string sdpFmtpLine;
+        for (auto& pair : codec.parameters)
+        {
+          if (!sdpFmtpLine.empty())
+            sdpFmtpLine += "; ";
+          sdpFmtpLine += pair.first + "=" + pair.second;
+        }
+        arr.Add(variant_t(sdpFmtpLine.c_str()));
+      } else {
+        arr.Add(variant_t());
+      }
+
+      // Initialize the variant
+      VARIANT var;
+      VariantInit(&var);
+      var.vt = VT_ARRAY | VT_VARIANT;
+      var.parray = arr.Detach();
+      //add to encodinsg
+      codecs.Add(var);
+    }
+    // Initialize the variant
+    VARIANT var;
+    VariantInit(&var);
+    var.vt = VT_ARRAY | VT_VARIANT;
+    var.parray = codecs.Detach();
+
+    //Add codecs to params
+    args.Add(var);
+  }
+
+  // Initialize the variant
+  VariantInit(params);
+  params->vt = VT_ARRAY | VT_VARIANT;
+  params->parray = args.Detach();
+
+  //Done
   return S_OK;
 };
 
